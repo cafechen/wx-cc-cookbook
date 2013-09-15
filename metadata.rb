3f80667f13d11d0b137518f94ff0f8fb08fed8a0
Ke Zhu <shawnzhu@pluto.local>
2013-09-15 11:26:39 +0800

    init import

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name             'wx-cc-cookbook'
+maintainer       'letoke.com'
+maintainer_email 'davidzhu.4.2@gmail.com'
+license          'All rights reserved'
+description      'Installs/Configures wx callcenter'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
